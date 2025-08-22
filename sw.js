122d05ae8d2f2bde5c8e82f403f7c8268b943118
ojy103031-oss <o.jy103031@gmail.com>
2025-08-23 00:01:12 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('pocalist-cache').then(cache => {
+      return cache.addAll(['/', '/manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
